4e061fa2ba1a560e16af8cc690e18766a7825f8f
boconnorb20 <80371646+boconnorb20@users.noreply.github.com>
2021-07-21 14:17:16 -0500

    created server.js, setting up to call dependacies and port listener

---
+++ server.js
@@ -0,0 +1,16 @@
+// Importing dependencies
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+// Server creation
+const app = express();
+
+// Set a port listener
+const PORT = process.env.PORT || 3000
+
+// Setting the createDataNote array
+let createDataNote = [];
+
+
+
